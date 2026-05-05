4fa9d6c2c2c7fc48a2ba3df84c00fde29e94a03c
powerBIspecialist <powerbi.specialist1@gmail.com>
2026-05-05 18:28:55 +0300

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,13 @@
+-- 1. Insert data into clients table
+INSERT INTO clients VALUES
+(1, 'ANNA'),
+(2, 'GEORGE'),
+(3, 'Maria'),
+(4, 'Alain');
+
+-- 2. Insert sample data into orders table
+INSERT INTO orders VALUES
+(101 ,1, 'Mouse'),
+(102, 2, 'Laptop'),
+(103, 1, 'Smartphone'),
+(104, 1, 'Mousepad');
